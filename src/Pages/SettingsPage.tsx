6a8892e1af5ece0b5e02d47b2d25c30466ae6768
Rosie <rosie.tianshen@gmail.com>
2023-01-23 12:31:28 -0800

    Updated settings page and routing

---
+++ src/Pages/SettingsPage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../components/NavBar";
+
+export default function Settings() {
+    return (
+        <div>
+            <Navbar/>
+            <h1>Settings</h1>
+            {/* account settings */}
+            {/* change name */}
+            {/* change username */}
+            {/* change email */}
+            {/* change password */}
+
+            {/* filters settings */}
+            <a href="/create-preferences"></a>
+
+        </div>
+    );
+}
