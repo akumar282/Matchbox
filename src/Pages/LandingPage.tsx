54ea34fe0d71cd7af3825b30d7dfe813d80e2b89
Rosie <rosie.tianshen@gmail.com>
2023-02-04 12:53:47 -0800

    Improved color styling and code organization, added state management for user email input

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -24,6 +24,19 @@ function submitEmail(){
 }
 
 export default function LandingPage() {
+  // login handling
+  const [isLoginOpen, setIsLoginOpen] = React.useState(false)
+  function NoAccount() {
+    setIsLoginOpen(false)
+  }
+
+  // signup handling
+  const [userEmail, setUserEmail] = React.useState("");
+  const handleEmailChange = (event) => {
+    setUserEmail(event.target.value);
+  }
+
+  // page animations
   Aos.init({
     duration: 2500,
     delay: 400,
@@ -34,95 +47,113 @@ export default function LandingPage() {
 
   return (
     <div >
-    <div className='top-container'>
-      <div className='header-stack'>
-        <div className='logobox'>
-          <span className='logo'>Matchbox</span>
+      <LandingPopupLogin trigger={isLoginOpen} setTrigger={setIsLoginOpen} setCreateOpen = {NoAccount}/>
+      <div className='top-container'>
+        <div className='header-stack'>
+          <div className='logobox'>
+            <span className='logo'>Matchbox</span>
+          </div>
+          <Stack className='nav-stack' direction='row'>
+            {/* TODO link scoll animation here later */}
+            {/* <Button
+              sx={{
+                color: '#000000',
+                fontSize: 'max(20px, 10px);',
+              }}
+            >
+              {' '}            
+              <Link to="#about" /> 
+              About Us
+            </Button> */}
+            {/* login button */}
+            <Button
+              sx={{
+                color: '#000000',
+                fontSize: 'max(20px, 10px);',
+              }}
+              onClick={() => setIsLoginOpen(true)}
+            >
+              {' '}
+              Login
+            </Button>
+          </Stack>
         </div>
-        <Stack className='nav-stack' direction='row'>
-          {/* TODO link scoll animation here later */}
-          {/* <Button
+        <div className='landing-container'>
+          <Stack
+            className='text-stack'
+            direction='column'
             sx={{
-              color: '#000000',
-              fontSize: 'max(20px, 10px);',
+              justifyContent: 'center',
+              width: '100%',
             }}
           >
-            {' '}            
-            <Link to="#about" /> 
-            About Us
-          </Button> */}
-        </Stack>
-      </div>
-      <div className='landing-container'>
-        <Stack
-          className='text-stack'
-          direction='column'
-          sx={{
-            justifyContent: 'center',
-            width: '100%',
-          }}
-        >
-          <h1 
-            className='title-text'          
-            data-aos='fade-down'
-            data-aos-delay='600'
-          > 
-            Matchbox: Bridging between ideas and execution 
-          </h1>
-          <p
-            className='sub-text'           
-            data-aos='fade-down'
-            data-aos-delay='800'
-          >
-            Matchbox is a project collaboration platform that curates
-            countless relevant projects in seconds. 
-          </p>
-        </Stack>
-        <img className='matchstick' src={illustration}         
-            data-aos='fade-right'
-            data-aos-delay='900'></img>
-      </div>
-    </div>     
-    <div className='about-section'>
-      <div className="about-container">
-        <div className='signup-stack'>
-        <h2 className='signText'>Sign up to get free access to preview upon release.</h2>
-        <div className='signup-entry'>
-          <TextField variant="filled"  multiline color="#FFFFFF" placeholder="Email" sx={{ backgroundColor: '#FFFFFF', width:'35em'}}/> 
-          <Button onClick={() => submitEmail()}
-            sx={{
-              backgroundColor: '#F68084',
-              width:'15%',
-              height :'55px',
-              fontSize:'max(14px, 5px);', '&:hover': {
-              backgroundColor:'#f59da0',
-              },
-              mx:'10px',
-            }}
-            variant='contained'
-          > 
-            Sign Up
-          </Button>
+            <h1 
+              className='title-text'          
+              data-aos='fade-down'
+              data-aos-delay='600'
+            > 
+              Matchbox: Bridging between ideas and execution 
+            </h1>
+            <p
+              className='sub-text'           
+              data-aos='fade-down'
+              data-aos-delay='800'
+            >
+              Matchbox is a project collaboration platform that curates
+              countless relevant projects in seconds. 
+            </p>
+          </Stack>
+          <img className='matchstick' src={illustration}         
+              data-aos='fade-right'
+              data-aos-delay='900'></img>
         </div>
-      </div>
-      {/* TODO about information to add about section in the future */}
-      {/* <div className="about-text-container">
-      <a href="#about"> </a>
+      </div>     
+      <div className='about-section'>
+        <div className="about-container">
+          <div className='signup-stack'>
+          <h2 className='signText'>Sign up to get free access to preview upon release.</h2>
+          <div className='signup-entry'>
+            <TextField 
+              variant="filled"  
+              multiline color="#FFFFFF" 
+              placeholder="Email" 
+              sx={{ backgroundColor: '#FFFFFF', width:'35em'}}
+              onChange = {handleEmailChange}  
+            /> 
+            <Button onClick={() => submitEmail()}
+              sx={{
+                backgroundColor: '#F68084',
+                width:'15%',
+                height :'55px',
+                fontSize:'max(14px, 5px);', '&:hover': {
+                backgroundColor:'#f59da0',
+                },
+                mx:'10px',
+              }}
+              variant='contained'
+            > 
+              Sign Up
+            </Button>
+          </div>
+        </div>
+        {/* TODO about information to add about section in the future */}
+        {/* <div className="about-text-container">
+        <a href="#about"> </a>
 
-      <p className='about-title-text' 
-      data-aos='fade-up'
-      data-aos-delay='100'>About us</p>
-      <p className='about-sub-text'
-      data-aos='fade-up'
-      data-aos-delay='200'>
-      {subtext} 
+        <p className='about-title-text' 
+        data-aos='fade-up'
+        data-aos-delay='100'>About us</p>
+        <p className='about-sub-text'
+        data-aos='fade-up'
+        data-aos-delay='200'>
+        {subtext} 
 
-      Matchbox is a platform connecting developers to open source projects and connecting project owners and organizations to qualified developers. 
-      Our mission is to empower developers and project owners by streamlining the process of finding and collaborating on open source projects. 
-      </p>
-      </div> */}          
-      </div>
-    </div>       
+        Matchbox is a platform connecting developers to open source projects and connecting project owners and organizations to qualified developers. 
+        Our mission is to empower developers and project owners by streamlining the process of finding and collaborating on open source projects. 
+        </p>
+        </div> */}          
+        </div>
+      </div>       
   </div>
   );
 }
