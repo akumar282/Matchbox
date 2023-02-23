49e22b8618d20f29a6faffc438633e7f87678c65
Rosie <rosie.tianshen@gmail.com>
2023-02-23 00:29:03 -0800

    Added constants file

---
+++ src/Pages/constants.ts
@@ -0,0 +1,67 @@
+export * from './constants'
+
+import { FrameworkTag, LanguageTag, SizeTag } from '../API'
+
+export type langBundle = {
+  value: string
+  label: string
+  enumMap: LanguageTag
+}
+
+export type frameworkBundle = {
+  value: string
+  label: string
+  enumMap: FrameworkTag
+}
+
+export type sizeBundle = {
+  value: string
+  label: string
+  enumMap: SizeTag
+}
+
+export const LanguageTags : langBundle[] = [
+  { value: "Java", label: "Java", enumMap: LanguageTag.JAVA },
+  { value: "C++", label: "C++", enumMap: LanguageTag.C_PLUS_PLUS },
+  { value: "C", label: "C", enumMap: LanguageTag.C},
+  { value: "Python", label: "Python", enumMap: LanguageTag.PYTHON },
+  { value: "C#", label: "C#", enumMap: LanguageTag.C_SHARP }, 
+  { value: "JavaScript", label: "JavaScript", enumMap: LanguageTag.JAVASCRIPT },
+  { value: "PHP", label: "PHP", enumMap: LanguageTag.PHP },
+  { value: "Ruby", label: "Ruby", enumMap: LanguageTag.RUBY },
+  { value: "HTML", label: "HTML", enumMap: LanguageTag.HTML },
+  { value: "CSS", label: "CSS", enumMap: LanguageTag.CSS },
+  { value: "Swift", label: "Swift", enumMap: LanguageTag.SWIFT },
+  { value: "Go", label: "Go", enumMap: LanguageTag.GO },
+  { value: "Rust", label: "Rust", enumMap: LanguageTag.RUST },
+  { value: "Kotlin", label: "Kotlin", enumMap: LanguageTag.KOTLIN },
+  { value: "Dart", label: "Dart", enumMap: LanguageTag.DART }, 
+  { value: "Scala", label: "Scala", enumMap: LanguageTag.SCALA }, 
+  { value: "TypeScript", label: "TypeScript", enumMap: LanguageTag.TYPESCRIPT },
+  { value: "SQL", label: "SQL", enumMap: LanguageTag.SQL }, 
+];
+
+export const FrameworkTags: frameworkBundle[] = [
+  { value: "React", label: "React", enumMap: FrameworkTag.REACT},
+  { value: "React Native", label: "React Native", enumMap: FrameworkTag.REACT_NATIVE },
+  { value: "Angular", label: "Angular", enumMap: FrameworkTag.ANGULAR },
+  { value: "Vue", label: "Vue", enumMap: FrameworkTag.VUE },
+  { value: "Node", label: "Node", enumMap: FrameworkTag.NODE },
+  { value: "Express", label: "Express", enumMap: FrameworkTag.EXPRESS },
+  { value: "WebSocketIO", label: "WebSocketIO", enumMap: FrameworkTag.WEB_SOCKET_IO },
+  { value: "Django", label: "Django", enumMap: FrameworkTag.DJANGO },
+  { value: "Flask", label: "Flask", enumMap: FrameworkTag.FLASK },
+  { value: "MongoDB", label: "MongoDB", enumMap: FrameworkTag.MONGO_DB },
+  { value: "MySQL", label: "MySQL", enumMap: FrameworkTag.MY_SQL },
+  { value: "PostgreSQL", label: "PostgreSQL", enumMap: FrameworkTag.POSTGRES_SQL },
+  { value: "Firebase", label: "Firebase", enumMap: FrameworkTag.FIREBASE },
+  { value: "AWS", label: "AWS", enumMap: FrameworkTag.AWS },
+  { value: "Azure", label: "Azure", enumMap: FrameworkTag.AZURE },
+  { value: "Heroku", label: "Heroku", enumMap: FrameworkTag.HEROKU },
+];
+
+export const SizeTags = [
+  { value: "Small", label: "Small", enumMap: SizeTag.MORE_FIVE },
+  { value: "Medium", label: "Medium", enumMap: SizeTag.MORE_TEN},
+  { value: "Large", label: "Large", enumMap: SizeTag.MORE_TWENTY_FIVE}
+];
