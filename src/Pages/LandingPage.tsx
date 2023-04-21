0937b959ccccdb279b7020fac3518141116f58f2
mohazahid <adyanzahid786@gmail.com>
2023-04-20 17:15:38 -0700

    calc font size

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,61 +1,64 @@
-import * as React from 'react'
-import './CSS/LandingPage.css'
-import illustration from '../img/landing-img.svg'
-import awsconfig from '../aws-exports'
-import {Amplify} from 'aws-amplify'
+import * as React from "react";
+import "./CSS/LandingPage.css";
+import illustration from "../img/landing-img.svg";
+import awsconfig from "../aws-exports";
+import { Amplify } from "aws-amplify";
 import { CreateNewsletterEmailModelPayload } from "../backend/types";
-import { createNewsletterEmail } from "../../src/backend/mutations/newsletterMutations"
-import ConfirmationPopup from '../components/ConfirmationPopup'
-import LandingPopupLogin from '../components/LandingPopupLogin';
-import LandingPopupConfirm from '../components/LandingPopupConfirm';
-import LandingPopupCreate from '../components/LandingPopupCreate';
-import { TextField } from '@mui/material'
-import Button from '@mui/material/Button'
-import Stack from '@mui/material/Stack'
+import { createNewsletterEmail } from "../../src/backend/mutations/newsletterMutations";
+import ConfirmationPopup from "../components/ConfirmationPopup";
+import LandingPopupLogin from "../components/LandingPopupLogin";
+import LandingPopupConfirm from "../components/LandingPopupConfirm";
+import LandingPopupCreate from "../components/LandingPopupCreate";
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import { useFormik } from "formik";
 import * as yup from "yup";
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-import { about } from './LandingPageData' 
-import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-Amplify.configure(awsconfig)
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { about } from "./LandingPageData";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+Amplify.configure(awsconfig);
 const payload: CreateNewsletterEmailModelPayload = {
   input: {
-    email: ''
-  }
-}
+    email: "",
+  },
+};
 const globalAuthState = {
-  isAuthenticated: 'false',
+  isAuthenticated: "false",
   username: null,
   email: null,
   token: null,
   uuid: null,
-  isLoggedin: 'false',
-  firstTime: 'false',
-  currentProjectindex: '0'
-}
+  isLoggedin: "false",
+  firstTime: "false",
+  currentProjectindex: "0",
+};
 function setLocalStorage() {
-  if (!localStorage.getItem('email')) {
-    window.localStorage.setItem('email', globalAuthState.email)
+  if (!localStorage.getItem("email")) {
+    window.localStorage.setItem("email", globalAuthState.email);
   }
-  if (!localStorage.getItem('isAuthenticated')) {
-  window.localStorage.setItem('isLoggedin', globalAuthState.isLoggedin)
+  if (!localStorage.getItem("isAuthenticated")) {
+    window.localStorage.setItem("isLoggedin", globalAuthState.isLoggedin);
   }
-  if (!localStorage.getItem('firstTime')) {
-  window.localStorage.setItem('firstTime', globalAuthState.firstTime)
+  if (!localStorage.getItem("firstTime")) {
+    window.localStorage.setItem("firstTime", globalAuthState.firstTime);
   }
-  if (!localStorage.getItem('uuid')) {
-  window.localStorage.setItem('uuid', globalAuthState.uuid)
+  if (!localStorage.getItem("uuid")) {
+    window.localStorage.setItem("uuid", globalAuthState.uuid);
   }
-  if (!localStorage.getItem('token')) {
-  window.localStorage.setItem('token', globalAuthState.token)
+  if (!localStorage.getItem("token")) {
+    window.localStorage.setItem("token", globalAuthState.token);
   }
-  if (!localStorage.getItem('username')) {
-  window.localStorage.setItem('username', globalAuthState.username) 
+  if (!localStorage.getItem("username")) {
+    window.localStorage.setItem("username", globalAuthState.username);
   }
-  if (!localStorage.getItem('currentProjectindex')) {
-  window.localStorage.setItem('currentProjectindex', globalAuthState.currentProjectindex)
+  if (!localStorage.getItem("currentProjectindex")) {
+    window.localStorage.setItem(
+      "currentProjectindex",
+      globalAuthState.currentProjectindex
+    );
   }
 }
 setLocalStorage();
@@ -65,20 +68,21 @@ export default function LandingPage() {
   // const handleEmailChange = (event : any) => {
   //   setUserEmail(event.target.value);
   // }
-  
+
   // email signup database mutation
-  async function sendToDatabase(values : any) {
+  async function sendToDatabase(values: any) {
     payload.input.email = values.email;
-    const request = await createNewsletterEmail(payload)
-      .catch(error => console.log(error));  
+    const request = await createNewsletterEmail(payload).catch((error) =>
+      console.log(error)
+    );
   }
-  
+
   // login popup handling
-  const [isLoginOpen, setIsLoginOpen] = React.useState(false)
+  const [isLoginOpen, setIsLoginOpen] = React.useState(false);
   const [isConfirmOpen, setIsConfirmOpen] = React.useState(false);
-  const [isCreateOpen, setIsCreateOpen] = React.useState(false)
-  const [isAuthOpen, setAuthOpen] = React.useState(false)
-  
+  const [isCreateOpen, setIsCreateOpen] = React.useState(false);
+  const [isAuthOpen, setAuthOpen] = React.useState(false);
+
   function NoAccount() {
     setIsLoginOpen(false);
     setIsCreateOpen(true);
@@ -95,7 +99,7 @@ export default function LandingPage() {
   //   console.log(State);
   //   Dispatch({isAuthenticated: true});
   // }
-  // React.useEffect(() => { 
+  // React.useEffect(() => {
   //     console.log(State.isAuthenticated);
   //     console.log(localStorage.getItem('isAuthenticated'));
   //   }, [State.isAuthenticated]);
@@ -127,20 +131,34 @@ export default function LandingPage() {
       formik.resetForm();
     },
   });
-  
+
   return (
-    <div className='landing'>
-     
-      <ConfirmationPopup trigger={isAuthOpen} setTrigger={setAuthOpen} setLoginOpen = {OpenLogin}/>
-      <LandingPopupLogin trigger={isLoginOpen} setTrigger={setIsLoginOpen} setCreateOpen = {NoAccount}/>
-      <LandingPopupConfirm trigger={isConfirmOpen} setTrigger={setIsConfirmOpen}/>
-      <LandingPopupCreate trigger={isCreateOpen} setTrigger={setIsCreateOpen} setAuthOpen = {OpenAuth}/>
-      <div className='top-container'>
-        <div className='header-stack'>
-          <div className='logobox'>
-            <span className='logo'>Matchbox</span>
+    <div className="landing">
+      <ConfirmationPopup
+        trigger={isAuthOpen}
+        setTrigger={setAuthOpen}
+        setLoginOpen={OpenLogin}
+      />
+      <LandingPopupLogin
+        trigger={isLoginOpen}
+        setTrigger={setIsLoginOpen}
+        setCreateOpen={NoAccount}
+      />
+      <LandingPopupConfirm
+        trigger={isConfirmOpen}
+        setTrigger={setIsConfirmOpen}
+      />
+      <LandingPopupCreate
+        trigger={isCreateOpen}
+        setTrigger={setIsCreateOpen}
+        setAuthOpen={OpenAuth}
+      />
+      <div className="top-container">
+        <div className="header-stack">
+          <div className="logobox">
+            <span className="logo">Matchbox</span>
           </div>
-          <Stack className='nav-stack' direction='row'>
+          <Stack className="nav-stack" direction="row">
             {/* TODO link scoll animation here later */}
             {/* <Button
               sx={{
@@ -153,116 +171,119 @@ export default function LandingPage() {
               About Us
             </Button> */}
             {/* login button */}
-            
 
             <Button
               sx={{
-                color: '#000000',
-                fontSize: 'max(20px, 10px);',
+                color: "#000000",
+                fontSize: "max(20px, 10px);",
               }}
               onClick={() => setIsLoginOpen(true)}
             >
-              {' '}
+              {" "}
               Login
             </Button>
             <Button
               sx={{
-                color: '#000000',
-                fontSize: 'max(20px, 10px);',
+                color: "#000000",
+                fontSize: "max(20px, 10px);",
               }}
               onClick={() => setIsCreateOpen(true)}
             >
-              {' '}
+              {" "}
               Get Started
             </Button>
           </Stack>
         </div>
-        <div className='landing-container'>
+        <div className="landing-container">
           <Stack
-            className='text-stack'
-            direction='column'
+            className="text-stack"
+            direction="column"
             sx={{
-              justifyContent: 'center',
-              width: '100%',
-              ml: '2%',
+              justifyContent: "center",
+              width: "100%",
+              ml: "2%",
             }}
           >
-            <h1 
-              className='title-text'          
-              data-aos='fade-down'
-              data-aos-delay='600'
-            > 
-              Matchbox: Bridging between ideas and execution 
-            </h1>
-            <p
-              className='sub-text'           
-              data-aos='fade-down'
-              data-aos-delay='800'
+            <h1
+              className="title-text"
+              data-aos="fade-down"
+              data-aos-delay="600"
             >
+              Matchbox: Bridging ideas and execution
+            </h1>
+            <p className="sub-text" data-aos="fade-down" data-aos-delay="800">
               Matchbox is a project collaboration platform that curates
-              countless relevant projects in seconds. 
+              countless relevant projects in seconds.
             </p>
           </Stack>
-          <img className='matchstick' src={illustration}         
-              data-aos='fade-right'
-              data-aos-delay='900'></img>
+          <img
+            className="matchstick"
+            src={illustration}
+            data-aos="fade-right"
+            data-aos-delay="900"
+          ></img>
         </div>
-        <div className='button-stack'>
-            </div>
-      </div>     
-      
-      <div className='about-section'>
-        <div className="about-container">
-          <div className='signup-stack'>
-          <h2 className='signText'>Sign up to get free access to preview upon release.</h2>
-          <div className='signup-entry'>
-            
-           <TextField
-              id="email"
-              name="email"
-              placeholder="Email"
-              variant='standard'
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-              InputProps = {{
-                disableUnderline: true,
-              }}
-              sx={{
-                justifyContent: 'center',
-                px: '1rem',
-                height: '3.5rem',
-                width: 'max(20em, 10px)',
-                backgroundColor: '#FFFFFF',
-                fontSize: '20px',
-                mr: '1rem',
-                
-              }}
-            />
-            <Button onClick={() => formik.handleSubmit()}
-              id='submit-btn'
-              disabled={formik.isSubmitting}
-                sx={{
-                  backgroundColor: '#F68084',
-                  width:'15%',
-                  height :'55px',
-                  fontSize:'max(14px, 5px);', '&:hover': {
-                  backgroundColor:'#f59da0',
-                  
-                  },
-                  mx:'10px',
+        <div className="button-stack"></div>
+      </div>
+
+      <div className="about-section">
+        <div className="signup-stack">
+          <h2 className="signText">
+            Sign Up To Get Access To Our News Letter.
+          </h2>
+          <div className="signup-entry">
+            <div className="TextFieldHolder">
+              <TextField
+                id="email"
+                name="email"
+                placeholder="Email"
+                variant="standard"
+                fullWidth
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                InputProps={{
+                  disableUnderline: true,
                 }}
-                variant='contained'
-              > 
-                Sign Up
-              </Button>
-            
-            
+                sx={{
+                  justifyContent: "center",
+                  px: "1rem",
+                  backgroundColor: "#FFFFFF",
+                  fontSize: "1vh",
+                }}
+              />
+            </div>
+            <Button
+              onClick={() => formik.handleSubmit()}
+              id="submit-btn"
+              disabled={formik.isSubmitting}
+              sx={{
+                backgroundColor: "#F68084",
+                width: "15%",
+                height: "6vh",
+                maxHeight: "4rem",
+                fontSize: "max(14px, 5px);",
+                "&:hover": {
+                  backgroundColor: "#f59da0",
+                },
+                mx: "10px",
+              }}
+              variant="contained"
+            >
+              Sign Up
+            </Button>
           </div>
         </div>
-        {/* TODO about information to add about section in the future */}
-        {/* <div className="about-text-container">
+      </div>
+    </div>
+  );
+}
+{
+  /* TODO about information to add about section in the future */
+}
+{
+  /* <div className="about-text-container">
         <a href="#about"> </a>
 
         <p className='about-title-text' 
@@ -276,9 +297,5 @@ export default function LandingPage() {
         Matchbox is a platform connecting developers to open source projects and connecting project owners and organizations to qualified developers. 
         Our mission is to empower developers and project owners by streamlining the process of finding and collaborating on open source projects. 
         </p>
-        </div> */}          
-        </div>
-      </div>       
-  </div>
-  );
+        </div> */
 }
