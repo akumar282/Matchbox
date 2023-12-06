c31d4e7a1c66fe2b08f79d99865352587c90deb2
Mohzahid <95829594+mohazahid@users.noreply.github.com>
2023-12-06 07:10:34 -0800

    Fix carosuel (#52)
    * Create LICENSE
    
    * done
    
    ---------
    
    Co-authored-by: Abhimanyu Kumar <73499369+akumar282@users.noreply.github.com>

--- src/Pages/DiscoverPage.tsx
+++ src/Pages/DiscoverPage.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import NavBar from '../components/NavBar'
 import DiscoverComponent from '../components/DiscoverComponent'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Navigation } from 'swiper/modules'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css'
+
 // import { Carousel } from '@material-tailwind/react'
-import { Carousel} from 'flowbite-react'
+import { Carousel} from '@material-tailwind/react'
 import { ThemeProvider } from '@material-tailwind/react'
 import {
   CloudProviderTag,
@@ -80,44 +86,71 @@ const ExampleData : PostsModel[] = [
 export default function DiscoverPage() {
   const [projectIndex, setProjectIndex] = useState<number>(0)
   const [projects, setProjects] = useState<React.ReactNode[]>([])
-
-  const goToNextIndex = () => {
-    setProjectIndex((prevIndex) => (prevIndex + 1) % projects.length)
-  }
-
-  const goToPreviousIndex = () => {
-    setProjectIndex((prevIndex) =>
-      prevIndex === 0 ? projects.length - 1 : prevIndex - 1
-    )
-  }
+  const [my_swiper, set_my_swiper] = useState({})
 
   useEffect(() => {
     const updatedProjects = ExampleData.map((x) => <DiscoverComponent key={x.id} data={x} />)
     setProjects(updatedProjects)
   }, [])
-
   return (
     <div className='flex flex-col min-h-screen'>
       <NavBar />
       <div className='flex flex-col flex-grow justify-between bg-primary-purple'>
-        <div className='flex-grow flex  justify-center'>
-          <div className='max-w-screen-2xl w-full'>
-            <Carousel className='' slide={false}>
-              {projects}
-            </Carousel>
+        <div className='flex-grow flex  justify-center overflow-visible'>
+          <div className='max-w-screen-2xl w-full z-10 overflow-visible'>
+            <Swiper
+              modules={[Pagination, Navigation]}
+              spaceBetween={50}
+              slidesPerView={1}
+              navigation ={{
+                nextEl:'.swiper-button-next',
+                prevEl:'.swiper-button-prev'
+              }}
+              loop={true}
+              pagination={{
+                clickable: true,
+                dynamicBullets: true,
+              }}
+              onSlideChange={(swiper) => {
+                setProjectIndex(swiper.activeIndex)
+              }}
+              onInit={(ev) => {
+                set_my_swiper(ev)
+              }}
+              className='relative z-10'
+            >
+              {projects.map((x, index) => (
+                <SwiperSlide className='z-1' key={index}>{x}</SwiperSlide>
+              ))}
+              <button className='absolute z-50 right-2 top-1/2 transform -translate-y-1/2 rounded-full bg-white p-2 shadow-md hidden lg:flex'
+                onClick={() => my_swiper.slideNext()}
+              > 
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                </svg>
+              </button>
+              <button className='absolute z-50 left-2 top-1/2 transform -translate-y-1/2 rounded-full bg-white p-2 shadow-md hidden lg:flex'
+                onClick={() => my_swiper.slidePrev()}
+              > 
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                </svg>
+              </button>
+            </Swiper>
           </div>
         </div>
+        
         <div className='bg-white grow-0 flex pt-3 static bottom-0 mt-4 justify-center w-full'>
           <div className='flex font-primary space-x-3 text-lg flex-row'>
             <button
               className='py-1 px-6 mb-3 bg-gray-300 hover:bg-gray-200 rounded-full'
-              onClick={goToPreviousIndex}
+              onClick={() => my_swiper.slidePrev()}
             >
               Back
             </button>
             <button
               className='py-1 px-6 mb-3 bg-blue-700 hover:bg-blue-400 rounded-full text-white'
-              onClick={goToNextIndex}
+              onClick={() => my_swiper.slideNext()}
             >
               Next
             </button>
@@ -126,6 +159,6 @@ export default function DiscoverPage() {
       </div>
     </div>
   )
-}
 
+}
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -11,8 +11,16 @@ export default function CommentSection(props: CommentSectionData) {
     <div className='lg:w-full w-[97%] bg-white rounded-lg mt-4'>
       <h1 className='ml-4 text-lg font-primary mt-2'>1 Comments</h1>
       <div className='flex flex-col items-center font-primary justify-center'>
-        <div className='w-full flex my-3 justify-center'>
-          <textarea className='rounded-lg w-[97%]' rows={1} cols={40} placeholder={'Write your comment...'}/>
+        <div onClick={e => {
+          e.stopPropagation()
+          e.currentTarget.focus()
+        }}  className='w-full flex my-3 justify-center'>
+          <textarea className='block rounded-lg w-[97%] z-20' 
+            onClick={e => {
+              e.stopPropagation()
+              e.currentTarget.focus()
+            }} 
+            rows={1} cols={40} placeholder={'Write your comment...'}/>
         </div>
         <div className='w-full flex lg:items-start lg:justify-start justify-center'>
           <button className='bg-blue-700 mb-3 lg:ml-4 lg:w-2/12 w-[97%] rounded-lg px-2 py-2 text-white'>Post Comment</button>
